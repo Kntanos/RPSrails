fe5e2966817eb248783342df53f482835b3a7114
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-08-08 10:53:53 +0100

    added game's code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,9 @@ class HomeController < ApplicationController
   def play; end
 
   def play_game
-    @game = RockPaperScissors.new()
+    @player_1 = Player.new(name: "Kostas")
+    @player_2 = Player.new(name: "Computer")
+    @game = RockPaperScissors.new(@player_1, @player_2)
     @game.play_round
   end
 
--- app/models/rock_paper_scissors.rb
+++ app/models/rock_paper_scissors.rb
@@ -1,6 +1,31 @@
 class RockPaperScissors
+    attr_accessor :person_1, :person_2
+        def initialize(player_1, player_2)
+        @person_1 = player_1
+        @person_2 = player_2
+        end
   
-    def play_round
-        "You played paper and Computer played scissors. Computer won!"
-    end
+        def play_round()
+          "You played #{person_1.choice} and Computer played #{person_2.choice}."
+          combo = { "scissors" => "paper", "paper" => "rock", "rock" => "scissors" }
+          if person_1.choice == person_2.choice
+           "It's a draw!"
+          elsif combo[person_1.choice] == person_2.choice
+            "Kostas won!"
+          else
+            "Computer won!"
+          end
+        end
 end
+
+    class Player
+        attr_accessor :name, :choice
+        def initialize(name)
+            @name = name
+            @choice = make_choice
+        end
+  
+        def make_choice
+            ["rock", "paper", "scissors"].sample
+        end
+    end
