238406bf7d787fd08251f6b220f034cb30b04418
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-08-16 14:18:04 +0100

    made params adjustment in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,12 @@ class HomeController < ApplicationController
   def index
   end
 
-  def play; end
+  def play
+    @player = Player.find_by(name: params[:name])
+  end
 
   def play_game 
-    @player_1 = Player.new(name: params[:name])
+    @player_1 = Player.find_by(name: params[:name])
     @player_2 = Player.new(name: "Computer")
     @game = RockPaperScissors.new(@player_1, @player_2)
     @game.play_round
